92ed480819b58415db9150a684cf220c7dd6d65d
Luka Ljubisavljevic <llj142631@stud.gibb.ch>
2024-09-27 00:44:46 +0200

    Added standard HTML to index.js for the root link

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ const numberRoute = require('./api/routes/number')
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send(`
+        <html>
+            <head>
+                <title>Welcome to my API</title>
+            </head>
+            <body>
+                <h1>Hey whoever!</h1>
+                <p>Everything you can do with this API:</p>
+                <ul>
+                    <li>/api/temp?celsius=25 for converting celsius to scientific unit named kelvin</li>
+                    <li>/prime/limit=10 for prime numbers</li>
+                    <li>/api/number?n=45 for Fibonacci numbers</li>
+                </ul>
+            </body>
+        </html>
+    `)
+})
+
 app.use('/api', tempRoute)
 app.use('/api', primeRoute)
 app.use('/api', numberRoute)
